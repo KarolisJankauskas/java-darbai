f72be0399e164ad6df67a8af0754686ecbedeb5e
karolisjankauskas <karolis121@yahoo.com>
2025-01-21 20:40:29 +0200

    init commit

---
+++ 02_2 exercises/src/MileToKilometer.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MileToKilometer {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter miles:");
+        double miles = scanner.nextDouble();
+
+        double kilometers = miles * 1.6;
+
+        System.out.println(miles + " miles is " + kilometers + " kilometers");
+
+        scanner.close();
+    }
+}
